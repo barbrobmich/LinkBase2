7fb63b40eb87bcb69b529343d74ded253f195240
Barbara Ristau <ristaub@gmail.com>
2017-04-08 18:18:33 -0700

    added launch screen and homepage, to show options for styling

---
+++ LinkBase2/HomeViewController.swift
@@ -0,0 +1,50 @@
+//
+//  HomeViewController.swift
+//  LinkBase2
+//
+//  Created by Barbara Ristau on 4/8/17.
+//  Copyright © 2017 Barbara Ristau. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        styleBackgroundImage()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func styleBackgroundImage() {
+        
+        // Add blur effect to background image
+        let blur = UIBlurEffect(style: UIBlurEffectStyle.light)
+        let blurEffectView = UIVisualEffectView(effect: blur)
+        blurEffectView.frame = backgroundImageView.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        backgroundImageView.addSubview(blurEffectView)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
