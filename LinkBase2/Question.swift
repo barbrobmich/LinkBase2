86c93ebc4700dffe3f8b488a2159b6498ace0daf
MiLeung <michaeleleung@gmail.com>
2017-04-12 20:04:01 -0700

    verbal question button and time

--- LinkBase2/Question.swift
+++ LinkBase2/Question.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class Question: NSObject {
 	var question: String?
+	var type: String?
+	var limit: Int?
 	var choices: [String]?
 	var correctAnswerIndex: Int?
-	
-	init(question: String, choices: [String], correctAnswerIndex: Int) {
+
+	init(question: String) {
 		self.question = question
+	}
+}
+
+class ChoiceQuestion: Question {
+	init(question: String, choices: [String], correctAnswerIndex: Int) {
+		super.init(question: question)
 		self.choices = choices
 		self.correctAnswerIndex = correctAnswerIndex
+		self.type = "ChoiceQuestion"
+	}
+}
+
+class VerbalQuestion: Question {
+	init(question: String, limit: Int) {
+		super.init(question: question)
+		self.limit = limit
+		self.type = "VerbalQuestion"
 	}
 }
